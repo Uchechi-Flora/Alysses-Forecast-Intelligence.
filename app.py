7fea9508a871dc8a6e919c57b9e2bd9095112337
Florapy <108020922+Uchechi-Flora@users.noreply.github.com>
2026-05-23 22:58:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ elif st.session_state.page == 'dashboard':
             paper_bgcolor='#fdfbfc', plot_bgcolor='#fdfbfc',
             margin=dict(l=20, r=20, t=95, b=40), height=380,
             legend=dict(orientation="h", yanchor="bottom", y=-0.4, xanchor="center", x=0.5, font=dict(size=11, color='#464646')
-        )
+        ))
         fig.update_xaxes(showgrid=True, gridcolor='#87AE73')
         fig.update_yaxes(showgrid=True, gridcolor='#87ae73', title="Units Sold")
         
